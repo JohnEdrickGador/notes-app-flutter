b9f04fd5a94708a788d84fe1438dc90e00443d08
JohnEdrickGador <jmgador@up.edu.ph>
2023-02-01 16:45:54 +0800

    Added email verification

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_course_beginner/firebase_options.dart';
 
 import 'views/login_view.dart';
 import 'views/register_view.dart';
+import 'views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,48 +43,20 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   print('Verified user');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const Text('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
           default:
             return const CircularProgressIndicator();
         }
       },
-    );
-  }
-}
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({super.key});
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Email verification"),
-      ),
-      body: Column(
-        children: [
-          const Text("Verify your email address"),
-          TextButton(
-            onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-
-              await user?.sendEmailVerification();
-            },
-            child: const Text('Send verification'),
-          ),
-        ],
-      ),
     );
   }
 }
