efdaaca5ecbb3702539d4f5420e0b80015382fd6
JohnEdrickGador <jmgador@up.edu.ph>
2023-02-03 13:55:48 +0800

    Created a dialog box for error handling in login_view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -73,9 +73,11 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  log("User not found");
+                  await showErrorDialog(context, "User not found");
                 } else if (e.code == "wrong-password") {
-                  log("Wrong password");
+                  await showErrorDialog(context, "Wrong credentials");
+                } else {
+                  await showErrorDialog(context, "Error: ${e.code.toString()}");
                 }
               }
 
@@ -97,4 +99,24 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
   }
+}
+
+Future<void> showErrorDialog(BuildContext context, String text) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('An error occurred'),
+        content: Text(text),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Ok'),
+          )
+        ],
+      );
+    },
+  );
 }
